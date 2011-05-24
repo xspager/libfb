9d826b22538887e349c479a3e85e7edd402139b6
Daniel Lemos <xspager@gmail.com>
2011-05-24 21:47:02 +0000

    foo

--- src/demos/text.c
+++ src/demos/text.c
@@ -1,7 +1,7 @@
 #include "../../include/font.h"
 #include "../lib/libfb.h"
 
-void draw_char(char);
+void draw_char(char, int);
 
 int char_pos = 0;
 
@@ -13,13 +13,14 @@ int main(int argc, char *argv[])
     lfb_init();
 
     for(i = 0; i < strlen(text); i++){
-        draw_char(text[i]);
+        draw_char(text[i], BLUE << i);
     }
 }
 
-void draw_char(char ch)
+void draw_char(char ch, int color)
 {
     char_pos++;
+
     int c, l;
     //char **font_char = font[ch];
 
@@ -29,7 +30,7 @@ void draw_char(char ch)
             for(c = 0; c < 8; c++){
                 //printf("%c", font[ch][l][c]);
                 if(font[ch][l][c] != ' '){
-                    lfb.setpixel(c+(char_pos*8)+((l+8*100)*lfb.width), BLUE);
+                    lfb.setpixel(c+(char_pos*8)+((l+8*100)*lfb.width), color);
                 }
                  
             }
