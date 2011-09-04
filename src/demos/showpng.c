efe79d52995f4c40700ef9b3503d7219f7551ce6
Daniel Lemos <xspager@gmail.com>
2011-09-04 16:19:10 -0300

    Fix for palette indexd png files and indentation fix

--- src/demos/showpng.c
+++ src/demos/showpng.c
@@ -25,12 +25,12 @@
 
 void abort_(const char * s, ...)
 {
-        va_list args;
-        va_start(args, s);
-        vfprintf(stderr, s, args);
-        fprintf(stderr, "\n");
-        va_end(args);
-        abort();
+	va_list args;
+	va_start(args, s);
+	vfprintf(stderr, s, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	abort();
 }
 
 int x, y;
@@ -46,94 +46,93 @@ png_bytep * row_pointers;
 
 void read_png_file(char* file_name)
 {
-        char header[8];    // 8 is the maximum size that can be checked
+	char header[8];	// 8 is the maximum size that can be checked
 
-        /* open file and test for it being a png */
-        FILE *fp = fopen(file_name, "rb");
-        if (!fp)
-                abort_("[read_png_file] File %s could not be opened for reading", file_name);
-        fread(header, 1, 8, fp);
-        if (png_sig_cmp(header, 0, 8))
-                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+	FILE *fp = fopen(file_name, "rb");
 
-
-        /* initialize stuff */
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-        if (!png_ptr)
-                abort_("[read_png_file] png_create_read_struct failed");
-
-        info_ptr = png_create_info_struct(png_ptr);
-        if (!info_ptr)
-                abort_("[read_png_file] png_create_info_struct failed");
-
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during init_io");
-
-        png_init_io(png_ptr, fp);
-        png_set_sig_bytes(png_ptr, 8);
-
-        png_read_info(png_ptr, info_ptr);
-
-        width = png_get_image_width(png_ptr, info_ptr);
-        height = png_get_image_height(png_ptr, info_ptr);
-        color_type = png_get_color_type(png_ptr, info_ptr);
-        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-        number_of_passes = png_set_interlace_handling(png_ptr);
-        png_read_update_info(png_ptr, info_ptr);
+	if (!fp)
+		abort_("[read_png_file] File %s could not be opened for reading", file_name);
+	
+	fread(header, 1, 8, fp);
+	
+	if (png_sig_cmp(header, 0, 8))
+		abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
 
 
-        /* read file */
-        if (setjmp(png_jmpbuf(png_ptr)))
-                abort_("[read_png_file] Error during read_image");
+	/* initialize stuff */
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 
-        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-        for (y=0; y<height; y++)
-                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+	if (!png_ptr)
+		abort_("[read_png_file] png_create_read_struct failed");
 
-        png_read_image(png_ptr, row_pointers);
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+		abort_("[read_png_file] png_create_info_struct failed");
 
-        fclose(fp);
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[read_png_file] Error during init_io");
+
+	png_init_io(png_ptr, fp);
+	png_set_sig_bytes(png_ptr, 8);
+
+	png_read_info(png_ptr, info_ptr);
+
+	width = png_get_image_width(png_ptr, info_ptr);
+	height = png_get_image_height(png_ptr, info_ptr);
+	color_type = png_get_color_type(png_ptr, info_ptr);
+	bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+	if(color_type == PNG_COLOR_TYPE_PALETTE)
+		png_set_expand(png_ptr);
+
+	number_of_passes = png_set_interlace_handling(png_ptr);
+	png_read_update_info(png_ptr, info_ptr);
+
+	/* read file */
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[read_png_file] Error during read_image");
+
+	row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+	for (y=0; y<height; y++)
+		row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+
+	png_read_image(png_ptr, row_pointers);
+
+	fclose(fp);
 }
 
 void process_file(void)
-{/*
-        if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGB)
-                abort_("[process_file] input file is PNG_COLOR_TYPE_RGB but must be PNG_COLOR_TYPE_RGBA "
-                       "(lacks the alpha channel)");
+{
+	int bytes_pp = 4;
 
-        if (png_get_color_type(png_ptr, info_ptr) != PNG_COLOR_TYPE_RGBA)
-                abort_("[process_file] color_type of input file must be PNG_COLOR_TYPE_RGBA (%d) (is %d)",
-                       PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
-*/
-        for (y=0; y<height; y++) {
-                png_byte* row = row_pointers[y];
-                for (x=0; x<width; x++) {
-                        png_byte* ptr = &(row[x*4]);
-						lfb.putpixel(x,y, ptr[0] << 16 | ptr[1] << 8 | ptr[2]);
-                        //printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
-                        //       x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
+	if (png_get_color_type(png_ptr, info_ptr) == PNG_COLOR_TYPE_RGB)
+		bytes_pp = 3;
+	else
+		if (png_get_color_type(png_ptr, info_ptr) != PNG_COLOR_TYPE_RGBA)
+			abort_("[process_file] color_type of input file must be PNG_COLOR_TYPE_RGBA (%d) (is %d)",
+				PNG_COLOR_TYPE_RGBA, png_get_color_type(png_ptr, info_ptr));
 
-                        /* set red value to 0 and green value to the blue one */
-                        //ptr[0] = 0;
-                        //ptr[1] = ptr[2];
-                }
-        }
+	for (y=0; y<height; y++) {
+		png_byte* row = row_pointers[y];
+		for (x=0; x<width; x++) {
+			png_byte* ptr = &(row[x * bytes_pp]);
+			lfb.putpixel(x,y, ptr[0] << 16 | ptr[1] << 8 | ptr[2]);
+		}
+	}
 }
 
 
 int main(int argc, char *argv[])
 {
-        if (argc != 2)
-                abort_("Usage: program_name <file>");
+	if (argc != 2)
+		abort_("Usage: program_name <file>");
 
-        read_png_file(argv[1]);
+	read_png_file(argv[1]);
 
-		lfb_init();
+	lfb_init();
 
-        process_file();
+	process_file();
 
-        return 0;
+	return 0;
 }
 
