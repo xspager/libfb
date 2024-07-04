2c799480993a3ed0a82f1c3b0b275c55ee986848
xpager <xspager@gmail.com>
2024-07-04 16:23:13 -0300

    Use corrent width and height. Also make olive-demo like the other demos.

--- src/demos/olive-demo.c
+++ src/demos/olive-demo.c
@@ -11,9 +11,9 @@ int main(void)
     lfb_init();
 
     lfb.refresh();
-    Olivec_Canvas oc = olivec_canvas((uint32_t *)lfb.scr, WIDTH, HEIGHT, 7680/4);
+    Olivec_Canvas oc = olivec_canvas((uint32_t *)lfb.scr, lfb.width, lfb.height, lfb.bytes_per_line / 4);
     // Taken from https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg
-    olivec_fill(oc, 0xFF00FF00);
-    olivec_circle(oc, WIDTH/2, HEIGHT/2, 180, 0xFF2D00BC);
+    olivec_fill(oc, 0xFFFFFFFF);
+    olivec_circle(oc, lfb.width/2, lfb.height/2, 180, 0xFFBC002D);
     lfb.refresh();
 }
